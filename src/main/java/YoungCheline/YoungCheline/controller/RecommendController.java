597fa0bd286cf7e652864440272ba62a9b10f47b
ehddbs4521 <ehddbs452100@naver.com>
2023-12-07 09:26:27 +0900

    fix: 추후 코드 수정

--- src/main/java/YoungCheline/YoungCheline/controller/RecommendController.java
+++ src/main/java/YoungCheline/YoungCheline/controller/RecommendController.java
@@ -6,6 +6,7 @@ import YoungCheline.YoungCheline.service.HotMenuServiceImpl;
 import YoungCheline.YoungCheline.service.RecommendServiceImpl;
 import YoungCheline.YoungCheline.service.SteadyMenuServiceImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -14,8 +15,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+@Slf4j
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
@@ -29,8 +32,13 @@ public class RecommendController {
         RecommendationResponseDto recommendationResponseDto = new RecommendationResponseDto();
         Map<String, String> error = new HashMap<>();
         error.put("error", "평가 횟수 5회 미만");
+
         if (recommendService.checkRecommend(authentication.getName())) {
             RecommendDto[] recommend = recommendService.getRecommend(authentication.getName());
+            for (int i = 0; i < recommend.length; i++) {
+                System.out.println(recommend[i].getMenuId());
+                System.out.println(recommend[i].getMenuName());
+            }
             RecommendDto[] hotMenu = hotMenuService.getHotMenu();
             RecommendDto[] steadyMenu = steadyMenuService.getSteadyMenu();
             recommendationResponseDto.setRecommend(recommend);
